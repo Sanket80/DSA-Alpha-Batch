bac1d1e9b42cc3a61b5a98b46655b0f287dedcdc
Sanket80 <sanketkadamsss4@gmail.com>
2024-06-11 17:17:57 +0530

    Zero One Triangle

--- Patterns/advance.java
+++ Patterns/advance.java
@@ -29,8 +29,22 @@ public class advance {
         }
     }
 
+    public static void zero_one_triangle(int n){
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]){
         //hollow_rectangle(4, 7);
-        inverted_rotated_pyramind(5);
+        //inverted_rotated_pyramind(5);
+        zero_one_triangle(5);
     }
 }
