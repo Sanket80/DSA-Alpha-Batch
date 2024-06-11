f477c5876c13d3bb45495df737fa4e78568e575d
Sanket80 <sanketkadamsss4@gmail.com>
2024-06-11 17:41:26 +0530

    Diamond

--- Patterns/advance.java
+++ Patterns/advance.java
@@ -89,11 +89,34 @@ public class advance {
             System.out.println();
         }
     }
+
+    public static void diamond(int n){
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=((2*i)-1) ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n ; i>=1 ; i--){
+            for(int j=1 ; j<=(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=((2*i)-1) ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]){
         //hollow_rectangle(4, 7);
         //inverted_rotated_pyramind(5);
         //zero_one_triangle(5);
         //butterfly(5);
-        solid_Rhombus(4);
+        //solid_Rhombus(4);
+        diamond(4);
     }
 }
