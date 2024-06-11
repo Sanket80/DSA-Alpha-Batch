931b235208b4926db5dc29e1dccac2376e3d8463
Sanket80 <sanketkadamsss4@gmail.com>
2024-06-11 20:12:16 +0530

    Array Pairs

---
+++ Arrays/pairs.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class pairs {
+    
+    public static void pairs(int arr[]){
+        int tp = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            int curr = arr[i];
+            for(int j=i+1 ; j<arr.length ; j++){
+                System.out.println("("+curr+","+arr[j]+")");
+                tp++;
+            }
+        }
+        System.out.println("Total Pairs: "+tp);
+    }
+
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,10};
+        pairs(arr);
+    }
+}
