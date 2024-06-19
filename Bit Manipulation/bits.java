d6799968cb0b308e10071d8f0ad0d2e6112f2e3e
Sanket80 <sanketkadamsss4@gmail.com>
2024-06-19 17:28:23 +0530

    Count Set Bits in a Number

--- Bit Manipulation/bits.java
+++ Bit Manipulation/bits.java
@@ -50,7 +50,18 @@ public class bits {
         return n & BitMask;
     }
 
+    public static int countsetBits(int n){
+        int count = 0;
+        while (n > 0) {
+            if((n & 1) == 1){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+
     public static void main(String args[]){
-        System.out.println(clearBitsinRange(15, 1, 2));
+        System.out.println(countsetBits(15));
     }
 }
