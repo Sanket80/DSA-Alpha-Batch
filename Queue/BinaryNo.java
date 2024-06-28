ad1ebc66ae390405d4d04b95b10dad8afc63356e
Sanket80 <sanketkadamsss4@gmail.com>
2024-06-28 20:44:43 +0530

    Print all Binary Nos till 'n'

---
+++ Queue/BinaryNo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BinaryNo {
+
+    public static void printBin(int n){
+        Queue<String> q = new LinkedList<>();
+        q.add("1");
+        n--;
+        while (n>=0) {
+            String s1 = q.peek();
+            q.remove();
+            System.out.println(s1);
+            String s2 = s1;
+            q.add(s1+"0");
+            q.add(s2+"1");
+            n--;
+        }
+    }
+    
+    public static void main(String args[]){
+        int n = 5;
+        printBin(n);
+    }
+}
