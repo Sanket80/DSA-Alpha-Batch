6a52c0110a7ed5c4bd2efa45c8584c6a10e7ad10
Sanket80 <sanketkadamsss4@gmail.com>
2024-07-05 17:10:42 +0530

    Build BST , Search in a BST

---
+++ Binary Trees/Binary Search Tree/BST.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class BST {
+
+    static class Node {
+        int data;
+        Node left, right;
+        
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root , int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static boolean Search(Node root , int key){
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == key){
+            return true;
+        }
+        if(root.data > key){
+            return Search(root.left, key);
+        }
+        else{
+            return Search(root.right, key);
+        }
+    }
+    
+    public static void inOrder(Node root){
+        if(root == null){
+            return ;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    public static void main(String args[]){
+        int values[] = {5,1,3,4,2,7};
+        Node root = null;
+
+        for(int i=0 ; i<values.length ; i++){
+            root = insert(root, values[i]);
+        }
+
+        inOrder(root);
+        System.out.println(Search(root, 3));
+    }
+}
