5b658d0bd4ca967d4d075eebd3aca6a5934978f7
Sanket80 <sanketkadamsss4@gmail.com>
2024-07-05 17:31:53 +0530

    Delete Node from BST

--- Binary Trees/Binary Search Tree/BST.java
+++ Binary Trees/Binary Search Tree/BST.java
@@ -42,6 +42,38 @@ public class BST {
             return Search(root.right, key);
         }
     }
+
+    public static Node inorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static Node delete(Node root , int val){
+        if(root.data > val){
+            root.left = delete(root.left, val);
+        }else if(root.data < val){
+            root.right = delete(root.right, val);
+        }else{
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+
+        }
+        return root;
+    }
     
     public static void inOrder(Node root){
         if(root == null){
@@ -53,7 +85,7 @@ public class BST {
     }
 
     public static void main(String args[]){
-        int values[] = {5,1,3,4,2,7};
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for(int i=0 ; i<values.length ; i++){
@@ -62,5 +94,8 @@ public class BST {
 
         inOrder(root);
         System.out.println(Search(root, 3));
+
+        root = delete(root, 10);
+        inOrder(root);
     }
 }
